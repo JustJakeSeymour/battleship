7396297f3f5f9569916e3e206f28d680a235c184
Kelsie Porter <krporter22@gmail.com>
2022-10-17 20:41:23 -0600

    Create sunk? and hit method and associated tests

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,10 +1,55 @@
 require 'pry'
-require './lib.ship'
+require './lib/ship'
 
 RSpec.describe Ship do 
-    it 'has a name' do 
-        cruiser = Ship.new("Cruiser", 3)
+  it 'has a name' do 
+    cruiser = Ship.new("Cruiser", 3)
 
-        expect(ship.name).to eq("Cruiser")
-    end
+    expect(cruiser).to be_instance_of(Ship)
+    expect(cruiser.name).to eq("Cruiser")
+  end
+
+  it 'ship has a length' do 
+    cruiser = Ship.new("Cruiser", 3)
+
+    expect(cruiser.length).to eq(3)
+  end
+
+  it 'has a health equal to its length by default' do 
+    cruiser = Ship.new("Cruiser", 3)
+
+    expect(cruiser.health).to eq(3)
+  end
+
+  it 'ship is not sunk by default' do 
+    cruiser = Ship.new("Cruiser", 3)
+
+    expect(cruiser.sunk?).to be (false)
+  end
+
+  it 'the ship can be hit and the health decreases' do
+    cruiser = Ship.new("Cruiser", 3)
+
+    cruiser.hit
+      
+    expect(cruiser.health).to eq(2)
+    expect(cruiser.length).to eq(3)
+    expect(cruiser.sunk?).to be(false)
+  end
+
+  it 'the ship is sunk when health reaches 0' do
+    cruiser = Ship.new("Cruiser", 3)
+
+    cruiser.hit
+    cruiser.hit
+      
+    expect(cruiser.health).to eq(1)
+    expect(cruiser.sunk?).to be(false)
+
+    cruiser.hit
+    
+    expect(cruiser.health).to eq(0)
+    expect(cruiser.sunk?).to be(true)
+  end
+    
 end
