b1f6d38e61b9463edbd1a91dbbf0b5babffe00dc
J Seymour <JustJakeSeymour@gmail.com>
2022-10-17 20:54:53 -0600

    update ship.rb with correct ship

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,5 +1,22 @@
-class Ship
-    def initialize(ship, length)
-    end
-    
+class Ship 
+  attr_reader :name,
+              :length,
+              :health
+
+  def initialize(name, length)
+    @name = name
+    @length = length
+    @health = length
+    @sunk = false
+  end
+
+  def sunk? 
+    @sunk
+  end
+
+  def hit 
+    @health -= 1
+    @sunk = true if @health == 0
+  end
+
 end
