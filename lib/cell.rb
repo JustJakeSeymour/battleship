05c8d5d3f532bcd5afc98b5d0be9750ab6b8c277
Kelsie Porter <krporter22@gmail.com>
2022-10-18 18:04:42 -0600

    Add render method

--- lib/cell.rb
+++ lib/cell.rb
@@ -21,11 +21,20 @@ class Cell
   end
   
   def fire_upon
+    render = false
     @fired_upon = true
     if @ship != nil
       @ship.hit
     else 'Miss!'
     end
+  end
+
+  def render(reveal_ship = false)
+    return "S" if reveal_ship == true
+    return "." if @fired_upon == false
+    return "M" if @fired_upon == true && @ship == nil
+    return "X" if @fired_upon == true && @ship.sunk?   
+    return "H" if @fired_upon == true && @ship != nil
   end
 
 end
