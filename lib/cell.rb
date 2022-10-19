ec0e061d7e34279dc2a54a106d5b161551cf3671
Kelsie Porter <krporter22@gmail.com>
2022-10-18 20:51:03 -0600

    Update render method

--- lib/cell.rb
+++ lib/cell.rb
@@ -25,16 +25,19 @@ class Cell
     @fired_upon = true
     if @ship != nil
       @ship.hit
-    else 'Miss!'
+    else 
+      'Miss!'
+      # You're not supposed to have what you want to happen on the same line as else
     end
   end
 
   def render(reveal_ship = false)
-    return "S" if reveal_ship == true
-    return "." if @fired_upon == false
-    return "M" if @fired_upon == true && @ship == nil
-    return "X" if @fired_upon == true && @ship.sunk?   
-    return "H" if @fired_upon == true && @ship != nil
+    return "S" if reveal_ship == true && @fired_upon == false && @ship 
+    # return "S" if reveal_ship && !@fired_upon? && @ship
+    return "." if !fired_upon?
+    return "M" if fired_upon? && !@ship
+    return "X" if fired_upon? && @ship.sunk?   
+    return "H" if fired_upon? && @ship
   end
 
 end
