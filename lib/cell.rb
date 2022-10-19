ce5ac3bdb514994bb53f2e2773ba74aa3df6d816
Kelsie Porter <krporter22@gmail.com>
2022-10-18 20:53:18 -0600

    Refactor fire_upon and render method

--- lib/cell.rb
+++ lib/cell.rb
@@ -23,17 +23,15 @@ class Cell
   def fire_upon
     render = false
     @fired_upon = true
-    if @ship != nil
+    if @ship 
       @ship.hit
     else 
       'Miss!'
-      # You're not supposed to have what you want to happen on the same line as else
     end
   end
 
   def render(reveal_ship = false)
-    return "S" if reveal_ship == true && @fired_upon == false && @ship 
-    # return "S" if reveal_ship && !@fired_upon? && @ship
+    return "S" if reveal_ship && !@fired_upon? && @ship
     return "." if !fired_upon?
     return "M" if fired_upon? && !@ship
     return "X" if fired_upon? && @ship.sunk?   
