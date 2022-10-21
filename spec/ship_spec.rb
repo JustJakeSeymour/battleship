7979ebad318e00f65a35b1b3b5e320e42f9c2568
J Seymour <JustJakeSeymour@gmail.com>
2022-10-20 21:14:28 -0600

    Refactor: through all spec files, prepared for merge

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,35 +1,26 @@
-require 'pry'
 require './lib/ship'
 
 RSpec.describe Ship do 
-  it 'has a name' do 
-    cruiser = Ship.new("Cruiser", 3)
+  let!(:cruiser) {Ship.new("Cruiser", 3)}
 
+  it 'has a name' do 
     expect(cruiser).to be_instance_of(Ship)
     expect(cruiser.name).to eq("Cruiser")
   end
 
   it 'ship has a length' do 
-    cruiser = Ship.new("Cruiser", 3)
-
     expect(cruiser.length).to eq(3)
   end
 
   it 'has a health equal to its length by default' do 
-    cruiser = Ship.new("Cruiser", 3)
-
     expect(cruiser.health).to eq(3)
   end
 
   it 'ship is not sunk by default' do 
-    cruiser = Ship.new("Cruiser", 3)
-
     expect(cruiser.sunk?).to be (false)
   end
 
   it 'the ship can be hit and the health decreases' do
-    cruiser = Ship.new("Cruiser", 3)
-
     cruiser.hit
       
     expect(cruiser.health).to eq(2)
@@ -38,8 +29,6 @@ RSpec.describe Ship do
   end
 
   it 'the ship is sunk when health reaches 0' do
-    cruiser = Ship.new("Cruiser", 3)
-
     cruiser.hit
     cruiser.hit
       
@@ -51,5 +40,4 @@ RSpec.describe Ship do
     expect(cruiser.health).to eq(0)
     expect(cruiser.sunk?).to be(true)
   end
-    
 end
