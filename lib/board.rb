d8ed5376bffe86227f0131273d5dcec5e28949f0
J Seymour <JustJakeSeymour@gmail.com>
2022-10-21 13:16:46 -0600

    fix: address pr comment to resolve

--- lib/board.rb
+++ lib/board.rb
@@ -26,7 +26,7 @@ class Board
     @cells.keys.include?(coordinate)
   end
 
-   def valid_placement?(ship, coordinates)
+  def valid_placement?(ship, coordinates)
     coordinates = coordinates.map {|coordinate| coordinate.upcase}
 
     if ship.length != coordinates.size || !are_all_coordinates_valid(coordinates)
@@ -45,52 +45,52 @@ class Board
 
     letters_or_numbers_sequential(letters, numbers)
   end
-  
-  def are_all_coordinates_valid(coordinates)
-    coordinates.all? do |coordinate|
-      valid_coordinate?(coordinate)
-    end
-  end
 
-  def letters_or_numbers_sequential(letters, numbers)
-    if letters.uniq.size == 1
-      if (numbers.first..numbers.last).to_a == numbers
-        return true
-      else
-        return false
-      end
-    elsif numbers.uniq.size == 1
-      if (letters.first..letters.last).to_a == letters
-        return true
-      else
-        return false
-      end
-    else 
-      false
-    end
+def are_all_coordinates_valid(coordinates)
+  coordinates.all? do |coordinate|
+    valid_coordinate?(coordinate)
   end
+end
 
-  def place(ship, coordinates)
-    coordinates = coordinates.map {|coordinate| coordinate.upcase}
-    if !valid_placement?(ship, coordinates) || !are_all_coordinates_empty(coordinates)
-      return "That is an invalid placement"
+def letters_or_numbers_sequential(letters, numbers)
+  if letters.uniq.size == 1
+    if (numbers.first..numbers.last).to_a == numbers
+      return true
+    else
+      return false
     end
-    coordinates.each {|coordinate| @cells[coordinate].place_ship(ship) }
+  elsif numbers.uniq.size == 1
+    if (letters.first..letters.last).to_a == letters
+      return true
+    else
+      return false
+    end
+  else 
+    false
   end
+end
 
-  def are_all_coordinates_empty(coordinates)
-    coordinates.all? do |coordinate|
-      @cells[coordinate].empty?
-    end
+def place(ship, coordinates)
+  coordinates = coordinates.map {|coordinate| coordinate.upcase}
+  if !valid_placement?(ship, coordinates) || !are_all_coordinates_empty(coordinates)
+    return "That is an invalid placement"
   end
+  coordinates.each {|coordinate| @cells[coordinate].place_ship(ship) }
+end
 
-  def render(reveal_ship = false)
-    if reveal_ship == true
-      board_render_reveal_ships
-    else reveal_ship == false
-      board_render_hide_ships
-    end
+def are_all_coordinates_empty(coordinates)
+  coordinates.all? do |coordinate|
+    @cells[coordinate].empty?
   end
+end
+
+def render(reveal_ship = false)
+  if reveal_ship == true
+    board_render_reveal_ships
+  else reveal_ship == false
+    board_render_hide_ships
+  end
+end
 
   def board_render_reveal_ships
     row_a_keys = @cells.keys.select {|key| key[0] == "A"}
@@ -106,15 +106,15 @@ class Board
   end
 
   def board_render_hide_ships
-      row_a_keys = @cells.keys.select {|key| key[0] == "A"}
-      row_a = row_a_keys.map {|key| @cells[key].render}
-      row_b_keys = @cells.keys.select {|key| key[0] == "B"}
-      row_b = row_b_keys.map {|key| @cells[key].render}
-      row_c_keys = @cells.keys.select {|key| key[0] == "C"}
-      row_c = row_c_keys.map {|key| @cells[key].render}
-      row_d_keys = @cells.keys.select {|key| key[0] == "D"}
-      row_d = row_d_keys.map {|key| @cells[key].render}
+    row_a_keys = @cells.keys.select {|key| key[0] == "A"}
+    row_a = row_a_keys.map {|key| @cells[key].render}
+    row_b_keys = @cells.keys.select {|key| key[0] == "B"}
+    row_b = row_b_keys.map {|key| @cells[key].render}
+    row_c_keys = @cells.keys.select {|key| key[0] == "C"}
+    row_c = row_c_keys.map {|key| @cells[key].render}
+    row_d_keys = @cells.keys.select {|key| key[0] == "D"}
+    row_d = row_d_keys.map {|key| @cells[key].render}
 
-      the_board = "  1 2 3 4 \n" + "A #{row_a.join(' ')} \n" + "B #{row_b.join(' ')} \n" + "C #{row_c.join(' ')} \n" + "D #{row_d.join(' ')}"
-    end
+    the_board = "  1 2 3 4 \n" + "A #{row_a.join(' ')} \n" + "B #{row_b.join(' ')} \n" + "C #{row_c.join(' ')} \n" + "D #{row_d.join(' ')}"
+  end
 end
