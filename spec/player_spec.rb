781c605d10dae805c65445e0e3a99764d5a5635f
J Seymour <JustJakeSeymour@gmail.com>
2022-10-21 15:21:58 -0600

    WIP: pause on player class testing

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -6,26 +6,50 @@ require 'pry'
 
 RSpec.describe Player do
   let!(:player) {Player.new}
+  
   it 'is a Player class' do
     expect(player).to be_a Player    
   end
+  
   it 'begins with an empty board' do
     expect(player.board).to be_a Board
   end
+  
   it 'begins with required ships' do
     expect(player.ships[0].name).to eq("Cruiser")
     expect(player.ships[1].name).to eq("Submarine")
   end
+  
   it 'can place required ships into board' do
     player.choose_placement(player.ships[0], ["A1", "A2", "A3"])
     expect(player.board.cells["A1"].ship).to eq (player.ships[0])
     player.choose_placement(player.ships[1], ["B1", "C1"])
     expect(player.board.cells["C1"].ship).to eq (player.ships[1])
   end
-  it 'denies placement if given bad coordinate / ship combo' do
-    
+  
+  xit 'denies placement if given bad coordinate / ship combo' do
+    # binding.pry
+    player.choose_placement(player.ships[0], ["A2", "A3"])
+    expect(player.board.cells["A1"].ship).to eq (player.ships[0])
+    player.choose_placement(player.ships[1], ["B1", "C1"])
+    expect(player.board.cells["C1"].ship).to eq (player.ships[1])
+  end
+  
+  it 'keeps prompting for coordinates until new value' do
     
   end
   
+  it 'can fire upon the computer board' do
+
+  end
+  
+  it 'denies coordinates if repeat' do
+    
+  end  
+
+  it 'keeps prompting for coordinates until new value' do
+    
+  end
+
   
 end
