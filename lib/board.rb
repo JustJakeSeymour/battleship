f751b46a36d3a88a37ed94939adcb89c7d2d467b
J Seymour <JustJakeSeymour@gmail.com>
2022-10-20 20:11:52 -0600

    begin refactor branch, use helper methods

--- lib/board.rb
+++ lib/board.rb
@@ -30,20 +30,28 @@ class Board
     @cells.keys.include?(coordinate)
     
   end
-
   def valid_placement?(ship, coordinates)
     coordinates = coordinates.map {|coordinate| coordinate.upcase}
-    # FIRST iterate valid coordinate per coordinate instance
-    are_all_coordinates_valid = coordinates.all? do |coordinate|
-      valid_coordinate?(coordinate)
-    end
 
-    # SECOND test ship size is the same as coordinate length given
+    length_and_coordinates
+    split_for_testing
+    test_sequential_coordinates  
+
+  end
+
+  def are_all_coordinates_valid 
+    coordinates.all? do |coordinate|
+    valid_coordinate?(coordinate)
+    end
+  end
+
+  def length_and_coordinates
     if ship.length != coordinates.size || !are_all_coordinates_valid
       return false
     end
+  end
 
-    # NOW splitting up the coordinates to get number and letter arrays
+  def split_for_testing
     split_coordinates = coordinates.map do |coordinate|
       coordinate.split('') 
     end
@@ -53,15 +61,15 @@ class Board
     numbers = split_coordinates.map do |coordinate|
       coordinate.last.to_i
     end
+  end
 
-    # This tests the letters are all the same, and if so the numbers are the range
-    if letters.uniq.size == 1
+def test_sequential_coordinates 
+  if letters.uniq.size == 1
       if (numbers.first..numbers.last).to_a == numbers
         return true
       else
         return false
       end
-    # This tests the numbers are all the same, and if so the letters are the range
     elsif numbers.uniq.size == 1
       if (letters.first..letters.last).to_a == letters
         return true
@@ -72,6 +80,47 @@ class Board
       false
     end
   end
+  # def valid_placement?(ship, coordinates)
+  #   coordinates = coordinates.map {|coordinate| coordinate.upcase}
+  #   # FIRST iterate valid coordinate per coordinate instance
+  #   are_all_coordinates_valid = coordinates.all? do |coordinate|
+  #     valid_coordinate?(coordinate)
+  #   end
+
+  #   # SECOND test ship size is the same as coordinate length given
+  #   if ship.length != coordinates.size || !are_all_coordinates_valid
+  #     return false
+  #   end
+
+  #   # NOW splitting up the coordinates to get number and letter arrays
+  #   split_coordinates = coordinates.map do |coordinate|
+  #     coordinate.split('') 
+  #   end
+  #   letters = split_coordinates.map do |coordinate|
+  #     coordinate.first
+  #   end
+  #   numbers = split_coordinates.map do |coordinate|
+  #     coordinate.last.to_i
+  #   end
+
+  #   # This tests the letters are all the same, and if so the numbers are the range
+  #   if letters.uniq.size == 1
+  #     if (numbers.first..numbers.last).to_a == numbers
+  #       return true
+  #     else
+  #       return false
+  #     end
+  #   # This tests the numbers are all the same, and if so the letters are the range
+  #   elsif numbers.uniq.size == 1
+  #     if (letters.first..letters.last).to_a == letters
+  #       return true
+  #     else
+  #       return false
+  #     end
+  #   else 
+  #     false
+  #   end
+  # end
   
   def place(ship, coordinates)
     # upcase coordinates for safety
