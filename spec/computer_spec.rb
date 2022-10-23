eb78a4f3b00d392b2239880a0e23a6fd00afde54
Kelsie Porter <krporter22@gmail.com>
2022-10-22 20:14:09 -0600

    Create test to validate ship placement

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -24,8 +24,8 @@ RSpec.describe Computer do
   end
 
   describe '#place_ships' do  
+    let!(:computer) {Computer.new}
     it 'computer places the ships in valid coordinates' do 
-      computer = Computer.new
 
       computer.board.cells.values.all? {|cell| cell.empty?}
       expect(computer.board.cells.values.all? {|cell| cell.empty?}).to be true
@@ -34,6 +34,30 @@ RSpec.describe Computer do
       expect(computer.board.cells.values.any? {|cell| !cell.empty?}).to be true
 
       expect(computer.board.cells.values.select {|cell| !cell.empty?}.size).to eq(5)
+
+      # This code finds where the cruiser is placed on the board since it gets placed randomly
+      # returns the cell instances. These are saved to cruiser_cells
+      cruiser_cells = computer.board.cells.values.select do |cell|
+        cell.ship == computer.cruiser
+      end
+      # This code finds where the submarine is placed on the board since it gets placed randomly
+      # returns the cell instances. These are saved to sub_cells
+      sub_cells = computer.board.cells.values.select do |cell|
+        cell.ship == computer.submarine
+      end
+      # This code takes the array of cell instances and returns an array of just the coordinates attribute
+      # of the cell instance. We do this because the valid_placement? method takes an argument of JUST coordinates, not the whole cell instance.
+      cruiser_coordinates = cruiser_cells.map do |cell|
+        cell.coordinates 
+      end
+      # Same as above 48-49 comment
+      sub_coordinates = sub_cells.map do |cell|
+        cell.coordinates
+      end
+      # Now we have validated that not only were the ships placed correctly on the board but the placements
+      # are also valid
+      expect(computer.board.valid_placement?(computer.cruiser, cruiser_coordinates)).to be true
+      expect(computer.board.valid_placement?(computer.submarine, sub_coordinates)).to be true
     end
   end
 
