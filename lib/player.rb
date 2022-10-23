291922a831853054d979e4303b987380235f7aa6
Kelsie Porter <krporter22@gmail.com>
2022-10-23 12:53:10 -0600

    Complete #fire_upon_computer and remove #place_ships

--- lib/player.rb
+++ lib/player.rb
@@ -1,35 +1,38 @@
-class Player
+require 'pry'
+require './lib/board'
+require './lib/ship'
 
-  attr_reader :board, :cruiser, :submarine, :ships
+class Player
+  attr_reader :board, 
+              :cruiser, 
+              :submarine
 
   def initialize
     @board = Board.new
     @cruiser = Ship.new("Cruiser", 3) 
     @submarine = Ship.new("Submarine", 2)
-    @ships = [ @cruiser, @submarine]
-  end
-  
-  # maybe the runner will loop placement until the ship array has been successfully placed. 
-  # like an each? for player.ships |ship| choose_placement(ship, GETS COORDINATES)
-  def place_ships(ship, coordinates)
-    if @board.valid_placement?(ship, coordinates)
-      @board.place(ship, coordinates)
-    else
-      "Please supply different coordinates."
-    end
+    @fired_upon_coordinates = []
   end
 
-  # player is prompted to give a coordinate, convert .to_s and apply to computer's board
-  def choose_fire_upon(coordinate)
-    # log coordinate into an array (maybe that can happen here) <- use this to verify future turn.
-    coordinate_history = []
-    if @board.valid_coordinate? && !coordinate_history.include?(coordinate)
-      # need to confirm how to reach computer variable, scope issue.
-      # @@computer.board.cells[coordinate].fire_upon
-      coordinate_history << coordinate
-    else
-      "Please supply different coordinates."
+    # player is prompted to give a coordinate
+  def fire_upon_computer(coordinate, computer_board)
+    # computer_board argument above should be an instance of the computer's board passed in during the turn method in the game class
+    valid_coordinate = fired_upon_coordinate(coordinate)
+     
+    computer_board.cells[valid_coordinate].fire_upon
+    # The above will either return "Hit!" or "Miss!" from the fire_upon method in the cell class
+  end
+
+  private 
+
+  def fired_upon_coordinate(coordinate) 
+    until !@fired_upon_coordinates.include?(coordinate)
+      puts "You have already fired upon this coordinate"
+      puts "Please enter a new coordinate"
+      coordinate = gets.chomp.upcase
     end
+    @fired_upon_coordinates << coordinate
+    coordinate
   end
   
 end
