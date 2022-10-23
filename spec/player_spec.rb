9d53e433d1659dd8c87339f2d1fdce268ee23e92
Kelsie Porter <krporter22@gmail.com>
2022-10-23 12:54:45 -0600

    Update and pass all tests

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,6 +2,7 @@ require './lib/player'
 require './lib/board'
 require './lib/cell'
 require './lib/ship'
+require './lib/computer'
 require 'pry'
 
 RSpec.describe Player do
@@ -16,45 +17,41 @@ RSpec.describe Player do
   end
   
   it 'begins with required ships' do
-    expect(player.ships[0].name).to eq("Cruiser")
-    expect(player.ships[1].name).to eq("Submarine")
-  end
-  
-  it 'can place required ships into board' do
-    player.choose_placement(player.ships[0], ["A1", "A2", "A3"])
-    expect(player.board.cells["A1"].ship).to eq (player.ships[0])
-    player.choose_placement(player.ships[1], ["B1", "C1"])
-    expect(player.board.cells["C1"].ship).to eq (player.ships[1])
-  end
-  
-  xit 'denies placement if given bad coordinate / ship combo' do
-    # binding.pry
-    player.choose_placement(player.ships[0], ["A2", "A3"])
-    expect(player.board.valid_coordinate?).to be false
-  end
-  
-  xit 'keeps prompting for coordinates until new value' do
-    # this probably makes more sense for the computer test
-  end
-  
-  it 'can fire upon the computer board' do
-
-  end
-  
-  it 'denies coordinates if repeat' do
-    
-  end  
-
-  xit 'keeps prompting for coordinates until new value' do
-    # this probably makes more sense for the computer test
+    expect(player.cruiser.name).to eq("Cruiser")
+    expect(player.submarine.name).to eq("Submarine")
   end
 
-  it 'renders player board' do
+  describe '#fire_upon_coordinate' do 
+    let!(:player) {Player.new}
+    let!(:computer) {Computer.new}
 
-  end
+    it 'can fire upon computers board' do 
+      cruiser = Ship.new("Cruiser", 3)
 
-  it 'renders player board after placement' do
+      are_all_cells_empty = computer.board.cells.values.all? do |cell|
+        cell.empty?
+      end
 
+      expect(are_all_cells_empty).to be true
+
+      any_fired_upon_cells = computer.board.cells.values.any? do |cell|
+        cell.fired_upon?
+      end
+
+      expect(any_fired_upon_cells).to be false
+
+      expect(player.fire_upon_computer("A1", computer.board)).to eq('Miss!')
+
+      any_fired_upon_cells = computer.board.cells.values.any? do |cell|
+        cell.fired_upon?
+      end
+
+      expect(any_fired_upon_cells).to be true
+
+      computer.board.place(cruiser, ["A2","A3","A4"])
+
+      expect(player.fire_upon_computer("A3", computer.board)).to eq("Hit!")
+    end
   end
   
 end
