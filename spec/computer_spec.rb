ee22c5b0f90992cb5c4c98731f38c8175c73934a
Kelsie Porter <krporter22@gmail.com>
2022-10-23 10:42:42 -0600

    Create tests for #fire_upon_player

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -3,6 +3,7 @@ require './lib/cell'
 require './lib/board'
 require './lib/ship'
 require './lib/computer'
+require './lib/player'
 
 RSpec.describe Computer do
   let!(:computer) {Computer.new}
@@ -61,11 +62,30 @@ RSpec.describe Computer do
     end
   end
 
-  describe '#fire_upon' do 
+  describe '#fire_upon_player' do 
     let!(:computer) {Computer.new}
-    # is this a good method name?
     it 'can fire upon players board' do 
+      player = Player.new
 
+      are_all_cells_empty = player.board.cells.values.all? do |cell|
+        cell.empty?
+      end
+
+      expect(are_all_cells_empty).to be true
+
+      any_fired_upon_cells = player.board.cells.values.any? do |cell|
+        cell.fired_upon?
+      end
+
+      expect(any_fired_upon_cells).to be false
+
+      expect(computer.fire_upon_player(player.board)).to eq('Miss!')
+
+      any_fired_upon_cells = player.board.cells.values.any? do |cell|
+        cell.fired_upon?
+      end
+
+      expect(any_fired_upon_cells).to be true
     end
   end
   
