fef93af02c47aa4fb00917c92d98c015b252f5dd
Kelsie Porter <krporter22@gmail.com>
2022-10-24 10:24:37 -0600

    Update #fire_upon_computer and #fired_upon_coordinate

--- lib/player.rb
+++ lib/player.rb
@@ -14,27 +14,21 @@ class Player
     @fired_upon_coordinates = []
   end
 
-  # place_ships method was removed and this action now occurs in the Game class file
-
-    # player is prompted to give a coordinate
   def fire_upon_computer(coordinate, computer_board)
-    # computer_board argument above should be an instance of the computer's board passed in during the turn method in the game class
-    valid_coordinate = fired_upon_coordinate(coordinate)
-     
-    computer_board.cells[valid_coordinate].fire_upon
-    # The above will either return "Hit!" or "Miss!" from the fire_upon method in the cell class
-  end
-
-  private 
-
-  def fired_upon_coordinate(coordinate) 
-    # add valid_coordinate? using until condition as well
     until @board.valid_coordinate?(coordinate)
       puts "That is not a valid coordinate"
       puts "Please enter a new coordinate"
       coordinate = gets.chomp.upcase
     end
-    # this check works for repeats, but we are missing the ability to check for valid_coordinate?
+
+    valid_coordinate = fired_upon_coordinate(coordinate)
+     
+    computer_board.cells[valid_coordinate].fire_upon
+  end
+
+  private 
+
+  def fired_upon_coordinate(coordinate) 
     until !@fired_upon_coordinates.include?(coordinate)
       puts "You have already fired upon this coordinate"
       puts "Please enter a new coordinate"
